7c54be37e0d66e26256d58335b78d28d2b0544c3
buxiasen <buxiasen@xiaomi.com>
2024-11-08 12:13:55 +0800

    blk/mtdoutstream: flush before close
    Signed-off-by: buxiasen <buxiasen@xiaomi.com>

--- libs/libc/stream/lib_mtdoutstream.c
+++ libs/libc/stream/lib_mtdoutstream.c
@@ -237,6 +237,7 @@ void lib_mtdoutstream_close(FAR struct lib_mtdoutstream_s *stream)
 
       if (stream->cache != NULL)
         {
+          mtdoutstream_flush(&stream->common);
           lib_free(stream->cache);
           stream->cache = NULL;
         }
